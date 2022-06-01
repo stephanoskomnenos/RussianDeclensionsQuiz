e615ec057099573095d252fab0097d263b169781
Steven Lin <linjt267@outlook.com>
2022-06-02 03:44:06 +0800

    [FIX] CRLF to LF

--- lib/utils/load_dict.dart
+++ lib/utils/load_dict.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 Future<List<List<String>>> loadDict(String fileName) async {
   final csvString = await rootBundle.loadString('assets/dict/$fileName');
-  const converter = CsvToListConverter(fieldDelimiter: '	');
+  const converter = CsvToListConverter(fieldDelimiter: '	', eol: '\n');
   final dictList = converter
       .convert(csvString)
       .map((line) => line.map((field) => field.toString()).toList())
